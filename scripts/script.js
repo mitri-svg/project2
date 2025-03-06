6ff0cea86f3a752bcf0a06c17b14f1cbf0e721d3
mitri-svg <hmirseid54@gmail.com>
2025-03-06 18:00:56 +0400

    a

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const start = document.querySelector('#start')
+const body = document.querySelector('body')
+
+start.onclick = () => {
+    body.innerHTML = `
+    <nav>
+
+
+    
+    <script src="scripts/script.js"></>
+
+    `
+};
